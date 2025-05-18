8697cf2f90754aea29bda69bc6bf5a0ce9fec784
ProkuratorPil <derevmit20061@ya.ru>
2025-05-18 11:13:30 +0300

    Попытка создать лабу

---
+++ MyWPFTest/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWPFTest.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyCollectionChanged
+    {
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+    }
+}
